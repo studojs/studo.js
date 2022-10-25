36334574c82915d32049a49686c517c4bc9ee46e
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2022-10-26 01:38:02 +0200

    chore: remove jest, husky

--- src/rest/restManager.ts
+++ src/rest/restManager.ts
@@ -1,4 +1,3 @@
-import { fetch } from 'cross-fetch';
 import { Client, randomInstallationId } from '../client';
 import {
   applicationId,
--- test/encoder.test.ts
+++
@@ -1,8 +0,0 @@
-import { decode, encode } from '../src/utils/encoder';
-
-it('should encode and decode b64 gzip', async () => {
-  const source = { hello: 'world' };
-  const encoded = await encode(source);
-  const decoded = await decode(encoded);
-  expect(decoded).toMatchObject(source);
-});
